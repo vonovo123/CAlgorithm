9a2f292fa4603b5204c5ec40d2fe7bae2d7ef2b9
vonovo123 <vonovo123@naver.com>
2020-10-05 10:57:04 +0900

    20201005

--- divideNconquer.h
+++ divideNconquer.h
@@ -3,4 +3,6 @@
 #include <string.h>
 #include <stdlib.h>
 void conqure(int n, int xStart, int yStart, int** array, int* result);
+void conqure1992(int n, int xStart, int yStart, int** array);
+void solve1992(int n, int** array);
 void solve2630();
